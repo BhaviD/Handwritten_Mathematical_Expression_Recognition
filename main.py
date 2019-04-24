458b78b36fe7781664a170b0f0f766faaf417c6c
git-aditi <aditi.shrivastava@students.iiit.ac.in>
2019-04-25 02:14:40 +0530

    Bound function for weight initialization added for Watcher

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ import sys
 import os
 import time
 import math
+
+#Bound function for weight initialisation
+def bound(self, nin, nout, kernel):
+    #bound(1, self.input_conv_filters, self.input_conv_kernel)
+    kernel_dim_1 = kernel[0]
+    kernel_dim_2 = kernel[1]
+    mul = kernel_dim_1  * kernel_dim_2
+    fin = nin * mul
+    fout = nout * mul
+    result = (6. / (fin + fout)
+    result = np.sqrt(result)
+    return result
