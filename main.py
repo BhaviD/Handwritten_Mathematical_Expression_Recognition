6b72ec4571bdc8d65a51258baf8e55560e8656d4
git-aditi <aditi.shrivastava@students.iiit.ac.in>
2019-04-28 14:58:27 +0530

    Resolved Merge Conflict

--- main.py
+++ main.py
@@ -6,18 +6,6 @@ import os
 import time
 import math
 
-#Bound function for weight initialisation
-def bound(self, nin, nout, kernel):
-    #bound(1, self.input_conv_filters, self.input_conv_kernel)
-    kernel_dim_1 = kernel[0]
-    kernel_dim_2 = kernel[1]
-    mul = kernel_dim_1  * kernel_dim_2
-    fin = nin * mul
-    fout = nout * mul
-    result = (6. / (fin + fout)
-    result = np.sqrt(result)
-    return result
-
 rng = np.random.RandomState(int(time.time()))
 
 def norm_weight(fan_in, fan_out):
@@ -45,6 +33,7 @@ class Watcher_train():
         self.input_conv_stride = input_conv_stride
         self.input_conv_kernel = input_conv_kernel
 
+    #Bound function for weight initialisation
     def bound(self, nin, nout, kernel):
         kernel_dim_1 = kernel[0]
         kernel_dim_2 = kernel[1]
