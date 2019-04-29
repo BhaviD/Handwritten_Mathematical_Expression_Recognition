fde373ac38147cb125f9a5265a2ec774a4548b2f
Bhavi Dhingra <bhavi.dhingra@students.iiit.ac.in>
2019-04-29 21:01:52 +0530

    Adding prepare_data() function

--- data.py
+++ data.py
@@ -94,3 +94,28 @@ def load_dict(dictFile):
 
     print('total words/phones',len(lexicon))
     return lexicon
+
+
+def prepare_data(images_x, seqs_y, n_words_src=30000,
+                 n_words=30000):
+
+    heights_x = [s.shape[1] for s in images_x]
+    widths_x = [s.shape[2] for s in images_x]
+    lengths_y = [len(s) for s in seqs_y]
+
+    n_samples = len(heights_x)
+    max_height_x = numpy.max(heights_x)
+    max_width_x = numpy.max(widths_x)
+    maxlen_y = numpy.max(lengths_y) + 1
+
+    x = numpy.zeros((n_samples, max_height_x, max_width_x, 1)).astype('float32')
+    y = numpy.zeros((maxlen_y, n_samples)).astype('int64') # the <eol> must be 0 in the dict !!!
+    x_mask = numpy.zeros((n_samples, max_height_x, max_width_x)).astype('float32')
+    y_mask = numpy.zeros((maxlen_y, n_samples)).astype('float32')
+    for idx, [s_x, s_y] in enumerate(zip(images_x, seqs_y)):
+        x[idx, :heights_x[idx], :widths_x[idx], :] = (numpy.moveaxis(s_x, 0, -1) / 255.) # [B, C, H, W] -> [B, H, W, C]
+        x_mask[idx, :heights_x[idx], :widths_x[idx]] = 1.
+        y[:lengths_y[idx], idx] = s_y
+        y_mask[:lengths_y[idx]+1, idx] = 1.
+
+    return x, x_mask, y, y_mask
